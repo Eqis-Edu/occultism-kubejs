c008aa1b6095b9997ecd1dd9f74bd548eb8d1ca6
Kli Kli <klikli@outlook.at>
2024-08-08 06:48:51 +0200

    feat: update to new ritual recipe features in occultism 1.146

--- src/main/java/com/klikli_dev/occultism_kubejs/RitualRecipeSchema.java
+++ src/main/java/com/klikli_dev/occultism_kubejs/RitualRecipeSchema.java
@@ -5,6 +5,8 @@ import dev.latvian.mods.kubejs.recipe.component.*;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
@@ -15,6 +17,7 @@ public interface RitualRecipeSchema {
     RecipeKey<List<Ingredient>> INGREDIENTS = IngredientComponent.INGREDIENT.asList().inputKey("ingredients");
     RecipeKey<String> RITUAL_TYPE = StringComponent.ID.inputKey("ritual_type").alt("ritualType").optional("occultism:craft").alwaysWrite();
     RecipeKey<String> ENTITY_TO_SUMMON = StringComponent.ID.inputKey("entity_to_summon").alt("entityToSummon").alt("summon").defaultOptional();
+    RecipeKey<TagKey<EntityType<?>>> ENTITY_TAG_TO_SUMMON = TagKeyComponent.ENTITY_TYPE.inputKey("entity_tag_to_summon").alt("entityTagToSummon").alt("summonTag").defaultOptional();
     RecipeKey<String> ENTITY_NBT = StringComponent.ANY.inputKey("entity_nbt").alt("entityNbt").defaultOptional();
     RecipeKey<Ingredient> ACTIVATION_ITEM = IngredientComponent.INGREDIENT.inputKey("activation_item").alt("activationItem");
     RecipeKey<String> PENTACLE_ID = StringComponent.ID.inputKey("pentacle_id").alt("pentacleId").alt("pentacle");
@@ -28,7 +31,7 @@ public interface RitualRecipeSchema {
 
     RecipeKey<?> ENTITY_TO_SACRIFICE =
             RecipeComponent.builder(
-                    new RecipeComponentBuilder.Key("tag", StringComponent.ID),
+                    new RecipeComponentBuilder.Key("tag", TagKeyComponent.ENTITY_TYPE),
                     new RecipeComponentBuilder.Key("display_name", StringComponent.ANY)
             )
             .inputKey("entity_to_sacrifice").alt("entityToSacrifice").alt("sacrifice")
@@ -43,7 +46,7 @@ public interface RitualRecipeSchema {
             // and now all the optionals...
             DURATION, SPIRIT_MAX_AGE, SPIRIT_JOB_TYPE, RITUAL_DUMMY, RITUAL_TYPE,
             // excluded keys last
-            ENTITY_TO_SUMMON, ENTITY_NBT, ENTITY_TO_SACRIFICE, ITEM_TO_USE, COMMAND
+            ENTITY_TO_SUMMON, ENTITY_TAG_TO_SUMMON, ENTITY_NBT, ENTITY_TO_SACRIFICE, ITEM_TO_USE, COMMAND
     );
 
 }
